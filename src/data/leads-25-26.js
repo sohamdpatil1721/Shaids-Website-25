d2a37aa493240d34d757915e3014392987427441
sohamdpatil1721 <sohamdpatil1721@gmail.com>
2025-09-28 13:29:32 +0530

    New Leads 25-26

---
+++ src/data/leads-25-26.js
@@ -0,0 +1,139 @@
+const currentLeads = [
+  {
+    name: "Ajay Jadhav",
+    designation: "President",
+    avatar: "./src/assets/images/team-members-25-26/Ajay Jadhav President img.png",
+    instagramUrl: "https://www.instagram.com/ajayyyyyys/",
+    linkedinUrl: "https://www.linkedin.com/in/ajayjadhav07 ",
+  },
+  {
+    name: "Aayush Thoke",
+    designation: "General Secretary",
+    avatar:
+      "./src/assets/images/team-members-25-26/Aayush Thoke GS img.png",
+    instagramUrl: "https://www.instagram.com/_aayush_3105/",
+    linkedinUrl: "https://www.linkedin.com/in/aayushthoke ",
+  },
+  {
+    name: "Janmashree Gaokar",
+    designation: "Co-General Secretary",
+    avatar:
+      "./src/assets/images/team-members-25-26/Janmashree Gaokar Co-GS img.png",
+    instagramUrl: "https://www.instagram.com/janmeshreee/",
+    linkedinUrl: "https://www.linkedin.com/in/janmeshree-g-55611326a ",
+  },
+  {
+    name: "Vidhan Chavan",
+    designation: "Treasurer",
+    avatar:
+      "./src/assets/images/team-members-25-26/Vidhan Chavan Treasurer img.png",
+    instagramUrl: "https://www.instagram.com/vidhanchavan__4067/",
+    linkedinUrl: "https://www.linkedin.com/in/vidhan-chavan-472804302 ",
+  },
+  {
+    name: "Shravan Bhatkar",
+    designation: "Convener",
+    avatar:
+      "./src/assets/images/team-members-25-26/Shravan Bhatkar Convener img.png",
+    instagramUrl: "https://www.instagram.com/__shravan_20/",
+    linkedinUrl: "https://www.linkedin.com/in/shravan-bhatkar ",
+  },
+  {
+    name: "Divesh Dalvi",
+    designation: "Tech Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Divesh Dalvi Tech Head img.png",
+    instagramUrl: "https://www.instagram.com/dalvi.divesh/",
+    linkedinUrl: "https://www.linkedin.com/in/divesh-dalvi ",
+  },
+  {
+    name: "Yukta Parab",
+    designation: "Graphics Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Yukta Parab Graphic head img.png",
+    instagramUrl: "https://www.instagram.com/yuktaparab_26/",
+    linkedinUrl: "https://www.linkedin.com/in/yukta-parab-18a174355 ",
+  },
+  {
+    name: "Rutuja Sawant",
+    designation: "Planning Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Rutuja Sawant Planning Headimg.png",
+    instagramUrl: "https://www.instagram.com/rutujasawant._/",
+    linkedinUrl: "https://www.linkedin.com/in/rutuja-sawant-3b77aa270 ",
+  },
+  {
+    name: "Atharva Vadak",
+    designation: "Publicity Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Atharva Vadak Publicity Head img.png",
+    instagramUrl: "https://www.instagram.com/atharva_.v/",
+    linkedinUrl: "https://www.linkedin.com/in/atharva-vadak-063407366 ",
+  },
+  {
+    name: "Dhanushka Govindwar",
+    designation: "Cultural Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Dhanushka Govindwar Cultural Head img.png",
+    instagramUrl: "https://www.instagram.com/dhanushka__10/",
+    linkedinUrl: "https://www.linkedin.com/in/dhanushka-pralhad-govindwar-76b9a9344/ ",
+  },
+  {
+    name: "Vaibhavi Patil",
+    designation: "Social Media Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Vaibhavi Patil Social Media Head img.png",
+    instagramUrl: "https://www.instagram.com/02.trouble/",
+    linkedinUrl: "https://www.linkedin.com/in/vaibhavi-patil-60513a314 ",
+  },
+  {
+    name: "Bhavesh Patil",
+    designation: "Sports Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Bhavesh Patil sports Head img.png",
+    instagramUrl: "https://www.instagram.com/bhavesh_.058/",
+    linkedinUrl: "https://www.linkedin.com/company/shaids-dmce/ ",
+  },
+  {
+    name: "Shrushti Korde",
+    designation: "Sports Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Shrushti Korde Sports Head img.png",
+    instagramUrl: "https://www.instagram.com/ti.shrushti/",
+    linkedinUrl: "https://www.linkedin.com/in/shrushti-korde-391a232bb ",
+  },
+  {
+    name: "Sakshi Bhoir",
+    designation: "Documentation Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Sakshi Bhoir Documentation Head img.png",
+    instagramUrl: "https://www.instagram.com/sakshibhoirr_08/",
+    linkedinUrl: "https://www.linkedin.com/in/sakshi-bhoir-867539312 ",
+  },
+  {
+    name: "Parth Gadre",
+    designation: "Documentation Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Parth Gadre Document Head.png",
+    instagramUrl: "https://www.instagram.com/_parthgadre_/",
+    linkedinUrl: "https://www.linkedin.com/company/shaids-dmce/ ",
+  },
+  {
+    name: "Devesh Mokashi",
+    designation: "Sponsorship Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Devesh Mokashi Sponsership Head img (1).png",
+    instagramUrl: "https://www.instagram.com/_devesh_1103/",
+    linkedinUrl: "https://www.linkedin.com/in/devesh-mokashi-955824368/",
+  },
+  {
+    name: "Ganraj Mali",
+    designation: "Sponsorship Head",
+    avatar:
+      "./src/assets/images/team-members-25-26/Ganraj Mali Sponsership Head img.png",
+    instagramUrl: "https://www.instagram.com/_.ganrajj._/",
+    linkedinUrl: "https://www.linkedin.com/in/ganraj-mali-8b34b0375 ",
+  },
+];
+
+export default currentLeads;
